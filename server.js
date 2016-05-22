a697584876d998c164d8bf4f682799c4e8bc0aea
lujjjh <lujjjh@gmail.com>
2016-05-23 02:25:45 +0800

    Initial commit

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-var restify = require('restify');
+var express = require('express');
+var docker = require('docker-browser-console');
+var WebSocketServer = require('ws').Server;
+var websocket = require('websocket-stream');
+var pump = require('pump');
 
-var server = restify.createServer();
+var app = express();
+var http = require('http').Server(app);
 
-server.get('/', function (req, res, next) {
-  res.send('Hello world!');
-  next();
+var wss = new WebSocketServer({ server: http });
+
+wss.on('connection', function (socket) {
+	var stream = websocket(socket);
+	pump(stream, docker('onedev/c'), stream);
+})
+
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+http.listen(3000, function () {
+	console.log('listening on *:3000');
 });
